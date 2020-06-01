080d9d681a92b621501188fd96ca70d12ea7f1be
Ameya Raje <ameyacraje@gmail.com>
2020-06-01 07:16:32 -0400

    added express and ready for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        // path: '/Users/ameyaraje/Desktop/ReactJS/React-Experiments/indecision-app/public',
-        // usage of 'path' makes more sense so the project is portable
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test: /\.s?css$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
+module.exports = (env) => {
+    const isProd = env === 'production';
+
+    return {
+        entry: './src/app.js',
+        output: {
+            // path: '/Users/ameyaraje/Desktop/ReactJS/React-Experiments/indecision-app/public',
+            // usage of 'path' makes more sense so the project is portable
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }]
+        },
+        devtool: isProd ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public')
+        }
+    };
 };
